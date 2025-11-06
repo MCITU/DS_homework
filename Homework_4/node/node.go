66219a4e4af6c5b99258cc7e3e7fce300132b70e
MCITU <koldenborgmads@gmail.com>
2025-11-06 13:47:04 +0100

    standing point

---
+++ Homework_4/node/node.go
@@ -0,0 +1,19 @@
+package node
+
+/*
+	Have an IP which is the channel that it retrieve data and listens on
+	node struct that has the parameters of both a server and client
+	request access to the critical section
+
+*/
+
+import "fmt"
+
+var (
+	ID int // server IP ?
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+}
